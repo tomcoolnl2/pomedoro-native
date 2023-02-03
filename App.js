619fa8d1ad46f3acdbd97e7177ca342fba1d5ef9
coolt <tom.cool@tomtom.com>
2023-02-03 18:32:21 +0100

    Enforce Node 16 (for now). Updated npm packages. Enabled Web view.

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useCallback, useEffect, useState } from 'react'
 import { StyleSheet, View } from 'react-native'
-import AsyncStorage from '@react-native-community/async-storage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { uuidv4 } from './src/utils/uuid'
 import { Timer } from './src/features/timer/Timer'
 import { Focus } from './src/features/focus/Focus'
@@ -12,83 +12,83 @@ import { FocusHistory } from './src/features/focus/FocusHistory'
 // expo requires a default exports
 export default function App() {
 
-  const [focusSubject, setFocusSubject] = useState(null)
-  const [focusHistory, setFocusHistory] = useState([])
+	const [focusSubject, setFocusSubject] = useState(null)
+	const [focusHistory, setFocusHistory] = useState([])
 
-  const saveFocusHistory = async () => {
-    try {
-      await AsyncStorage.setItem('focusHistory', JSON.stringify(focusHistory))
-    } catch (e) {
-      console.log(e)
-    }
-  }
+	const saveFocusHistory = async () => {
+		try {
+			await AsyncStorage.setItem('focusHistory', JSON.stringify(focusHistory))
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-  const loadFocusHistory = async () => {
-    try {
-      const history = await AsyncStorage.getItem('focusHistory')
-      const focusHistory = JSON.parse(history)
-      if (history && focusHistory.length) {
-        setFocusHistory(focusHistory)
-      }
-    } catch (e) {
-      console.log(e)
-    }
-  }
+	const loadFocusHistory = async () => {
+		try {
+			const history = await AsyncStorage.getItem('focusHistory')
+			const focusHistory = JSON.parse(history)
+			if (history && focusHistory.length) {
+				setFocusHistory(focusHistory)
+			}
+		} catch (e) {
+			console.error(e)
+		}
+	}
 
-  useEffect(() => {
-    loadFocusHistory()
-  }, [])
+	useEffect(() => {
+		loadFocusHistory()
+	}, [])
 
-  useEffect(() => {
-    console.log(focusHistory)
-    saveFocusHistory()
-  }, [focusHistory])
+	useEffect(() => {
+		console.log(focusHistory)
+		saveFocusHistory()
+	}, [focusHistory])
 
-  const clearSubjectHandler = useCallback(() => {
-    setFocusHistory([
-      ...focusHistory,
-      { subject: focusSubject, status: 0, key: uuidv4() }
-    ])
-    setFocusSubject(null)
-  }, [focusHistory])
+	const clearSubjectHandler = useCallback(() => {
+		setFocusHistory([
+			...focusHistory,
+			{ subject: focusSubject, status: 0, key: uuidv4() }
+		])
+		setFocusSubject(null)
+	}, [focusHistory])
 
-  const onTimerEndHandler = useCallback(() => {
-    setFocusHistory([
-      ...focusHistory,
-      { subject: focusSubject, status: 1, key: uuidv4() }
-    ])
-    setFocusSubject(null)
-  }, [focusHistory])
+	const onTimerEndHandler = useCallback(() => {
+		setFocusHistory([
+			...focusHistory,
+			{ subject: focusSubject, status: 1, key: uuidv4() }
+		])
+		setFocusSubject(null)
+	}, [focusHistory])
 
-  return (
-    <View style={styles.container}>
-      <StatusBar style='light' />
-      {focusSubject 
-      ? (
-        <Timer
-          subject={focusSubject}
-          clearSubject={clearSubjectHandler}
-          onTimerEnd={onTimerEndHandler}
-        />
-      ) : (
-        <View style={styles.focusContainer}>
-          <Focus addSubject={setFocusSubject} />
-          <FocusHistory
-            focusHistory={focusHistory}
-            setFocusHistory={setFocusHistory}
-          />
-        </View>
-      )}
-    </View>
-  )
+	return (
+		<View style={styles.container}>
+			<StatusBar style='light' />
+			{focusSubject 
+			? (
+				<Timer
+					subject={focusSubject}
+					clearSubject={clearSubjectHandler}
+					onTimerEnd={onTimerEndHandler}
+				/>
+			) : (
+				<View style={styles.focusContainer}>
+					<Focus addSubject={setFocusSubject} />
+					<FocusHistory
+						focusHistory={focusHistory}
+						setFocusHistory={setFocusHistory}
+					/>
+				</View>
+			)}
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  focusContainer: { 
-    flex: 1, 
-    backgroundColor: '#252250'
-  }
+	container: {
+		flex: 1,
+	},
+	focusContainer: { 
+		flex: 1, 
+		backgroundColor: '#252250'
+	}
 })
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Sample Snack app
+# Sample App
+
+## Node version
+v16.x.x
 
 Open the `App.js` file to start writing some code. You can preview the changes directly on your phone or tablet by clicking the **Run** button or use the simulator by clicking **Tap to Play**. When you're done, click **Save** and share the link!
 
